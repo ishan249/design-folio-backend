acf93dc03b5ed44a8753e45eb5e2f28c3c062b5f
ishan249 <249patelishan@gmail.com>
2023-03-04 23:57:03 +0530

    new change

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const userRouter = require("./routers/user-routers.js");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:3000", "https://designfolio.onrender.com"],
+}));
 app.use(express.json());
 app.use(
   express.urlencoded({
